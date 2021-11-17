ecdd6a81d2b7dd84bfba391cc329192418dee53f
Piensom <56769500+piensom@users.noreply.github.com>
2021-11-17 13:28:32 -0600

    Update pos_order.py

--- pos_fel/models/pos_order.py
+++ pos_fel/models/pos_order.py
@@ -6,10 +6,10 @@ import logging
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
-    firma_fel = fields.Char('Firma FEL', related='account_move.firma_fel')
-    serie_fel = fields.Char('Serie FEL', related='account_move.serie_fel')
-    numero_fel = fields.Char('Numero FEL', related='account_move.numero_fel')
-    certificador_fel = fields.Char('Certificador FEL', related='account_move.certificador_fel')
+    firma_fel = fields.Char('Firma FEL', related='invoice_id.firma_fel')
+    serie_fel = fields.Char('Serie FEL', related='invoice_id.serie_fel')
+    numero_fel = fields.Char('Numero FEL', related='invoice_id.numero_fel')
+    certificador_fel = fields.Char('Certificador FEL', related='invoice_id.certificador_fel')
     
     def _prepare_invoice_line(self, order_line):
         res = super(PosOrder, self)._prepare_invoice_line(order_line)
@@ -20,8 +20,8 @@ class PosOrder(models.Model):
 
     def _prepare_invoice_vals(self):
         res = super(PosOrder, self)._prepare_invoice_vals()
-        if self.pedido_origen_id and self.pedido_origen_id.account_move:
-            res['factura_original_id'] = self.pedido_origen_id.account_move.id
+        if self.pedido_origen_id and self.pedido_origen_id.invoice_id:
+            res['factura_original_id'] = self.pedido_origen_id.invoice_id.id
             res['motivo_fel'] = 'Anulación'
         logging.warn(res)
         return res
