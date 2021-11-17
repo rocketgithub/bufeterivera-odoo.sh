505d808dae14f2ce6184c208189e184707565e86
Piensom <56769500+piensom@users.noreply.github.com>
2021-11-17 14:05:14 -0600

    Add files via upload

--- pos_fel/static/src/js/pos_fel.js
+++ pos_fel/static/src/js/pos_fel.js
@@ -19,41 +19,17 @@ screens.ReceiptScreenWidget.include({
             method: 'search_read',
             args: [[['pos_reference', '=', order.name]], ["firma_fel", "serie_fel", "numero_fel", "certificador_fel"]],
         }, {
-            timeout: 5000,
+            timeout: 6000,
         }).then(function (orders) {
             if (orders.length > 0) {
                 var env = self.get_receipt_render_env();
-
                 env['firma_fel'] = orders[0].firma_fel;
                 env['serie_fel'] = orders[0].serie_fel;
                 env['numero_fel'] = orders[0].numero_fel;
                 env['certificador_fel'] = orders[0].certificador_fel;
-
-                var precio_total_descuento = 0;
-                var precio_total_positivo = 0;
-
-                env['orderlines'].forEach(function(linea) {
-                    if (linea.get_unit_price() > 0) {
-                        precio_total_positivo += linea.get_price_with_tax();
-                    } else if (linea.get_unit_price() < 0) {
-                        precio_total_descuento += linea.get_price_with_tax();
-                    }
-                });
+                self.$('.pos-receipt-container').html(QWeb.render('PosTicket', env));
+                // this.$('.pos-receipt-container').html(QWeb.render('PosTicket', this.get_receipt_render_env()));
                 
-                env['precio_total_descuento'] = precio_total_descuento;
-
-                var descuento_porcentaje_fel = precio_total_descuento / precio_total_positivo * -1;
-                env['orderlines'].forEach(function(linea) {
-                    if (linea.get_unit_price() > 0) {
-                        linea.descuento_porcentaje_fel = descuento_porcentaje_fel * 100;
-                        linea.descuento_nominal_fel = linea.get_price_with_tax() * descuento_porcentaje_fel;
-                    } else if (linea.get_unit_price() < 0) {
-                        linea.descuento_porcentaje_fel = 100;
-                        linea.descuento_nominal_fel = linea.get_price_with_tax();
-                    }
-                });
-
-                self.$('.pos-receipt-container').html(QWeb.render('OrderReceipt', env));
             }
         });
     }
